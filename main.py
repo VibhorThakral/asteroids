2d4542debd460e8b0fe3a10f2f628e8e0c636dc6
vibhor <vibhorwebsolutions@gmail.com>
2025-02-20 10:49:00 +0530

    Added shooting functionality using spacebar

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from constants import *
 from player import Player
 from asteroid import Asteroid
 from asteroidfield import AsteroidField
+from shot import Shot
 
 def main():
   pygame.init()
@@ -12,10 +13,13 @@ def main():
   updatable = pygame.sprite.Group()
   drawable = pygame.sprite.Group()
   asteroids = pygame.sprite.Group()
+  shots = pygame.sprite.Group()
 
   Asteroid.containers = (asteroids, updatable, drawable)
   AsteroidField.containers = updatable
   asteroid_field = AsteroidField()
+
+  Shot.containers = (updatable, drawable)  
 
   Player.containers = (updatable, drawable)
   player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
