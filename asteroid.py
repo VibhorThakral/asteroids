a250562a1159371880d64cbe7b20b7fd440ab8a3
vibhor <vibhorwebsolutions@gmail.com>
2025-02-20 12:12:29 +0530

    Added split system on shooting

--- asteroid.py
+++ asteroid.py
@@ -1,5 +1,7 @@
 import pygame
+import random
 from circleshape import CircleShape
+from constants import ASTEROID_MIN_RADIUS
 
 class Asteroid(CircleShape):
   def __init__(self,x,y,radius):
@@ -10,3 +12,13 @@ class Asteroid(CircleShape):
 
   def update(self, dt):
     self.position += self.velocity * dt
+
+  def split(self):
+    self.kill()
+    if self.radius <= ASTEROID_MIN_RADIUS:
+      return      
+    random_angle = random.uniform(20,50)
+    ast_one = Asteroid(self.position.x, self.position.y, self.radius - ASTEROID_MIN_RADIUS)
+    ast_two = Asteroid(self.position.x, self.position.y, self.radius - ASTEROID_MIN_RADIUS)
+    ast_one.velocity = pygame.math.Vector2.rotate(self.velocity, random_angle) * 1.2
+    ast_two.velocity = pygame.math.Vector2.rotate(self.velocity, -random_angle) * 1.2
