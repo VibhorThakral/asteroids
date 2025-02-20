9644c5fc334d8cb8472e07d851b8fed5a8e3ca06
vibhor <vibhorwebsolutions@gmail.com>
2025-02-20 09:44:53 +0530

    Spawning asteroids in the asteroid field

--- main.py
+++ main.py
@@ -1,17 +1,26 @@
 import pygame
 from constants import *
 from player import Player
+from asteroid import Asteroid
+from asteroidfield import AsteroidField
 
 def main():
-  pygame.init()  
+  pygame.init()
   screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-  clock = pygame.time.Clock()  
-  dt = 0
+  clock = pygame.time.Clock()
 
   updatable = pygame.sprite.Group()
-  drawable = pygame.sprite.Group()  
+  drawable = pygame.sprite.Group()
+  asteroids = pygame.sprite.Group()
+
+  Asteroid.containers = (asteroids, updatable, drawable)
+  AsteroidField.containers = updatable
+  asteroid_field = AsteroidField()
+
   Player.containers = (updatable, drawable)
-  player = Player(SCREEN_WIDTH/2, SCREEN_HEIGHT/2)
+  player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+
+  dt = 0
   
 
   while True:
@@ -19,7 +28,7 @@ def main():
       if event.type == pygame.QUIT:
         return
       
-    updatable.update(dt)
+    updatable.update(dt)    
 
     screen.fill("black")  
     for sprite in drawable:
